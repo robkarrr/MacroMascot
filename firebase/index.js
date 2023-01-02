f60233138b643a94f24ba679fa39e1744a744885
robkarrr <93181892+robkarrr@users.noreply.github.com>
2023-01-02 15:02:18 +0100

    firebase innit

---
+++ firebase/index.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "firebase/app"
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+// Get Authentication instance
+const auth = getAuth(app)
+
+// Get Firestore instance
+const db = getFirestore(app)
+
+// Get Storage instance
+const storage = getStorage(app)
+
+export {
+	app as default,
+	auth,
+	db,
+	storage,
+}
