75c16f55f97b127c42ce7eeb8b38c699e4ffdf5e
robkarrr <93181892+robkarrr@users.noreply.github.com>
2023-01-09 21:58:38 +0100

    added meals page

---
+++ pages/meals.jsx
@@ -0,0 +1,147 @@
+import { useState } from "react"
+import { createStyles, Navbar, Group, Code, Button } from "@mantine/core"
+import useGetMeals from "../hooks/useGetMeals"
+import { useAuthContext } from "../context/AuthContext"
+import {
+  IconBellRinging,
+  IconFingerprint,
+  IconKey,
+  IconSettings,
+  Icon2fa,
+  IconDatabaseImport,
+  IconReceipt2,
+  IconSwitchHorizontal,
+  IconLogout,
+} from "@tabler/icons"
+import useGetUser from "../hooks/useGetUser"
+
+const useStyles = createStyles((theme, _params, getRef) => {
+  const icon = getRef("icon")
+  return {
+    header: {
+      paddingBottom: theme.spacing.md,
+      marginBottom: theme.spacing.md * 1.5,
+      borderBottom: `1px solid ${
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[4]
+          : theme.colors.gray[2]
+      }`,
+    },
+
+    footer: {
+      paddingTop: theme.spacing.md,
+      marginTop: theme.spacing.md,
+      borderTop: `1px solid ${
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[4]
+          : theme.colors.gray[2]
+      }`,
+    },
+
+    link: {
+      ...theme.fn.focusStyles(),
+      display: "flex",
+      alignItems: "center",
+      textDecoration: "none",
+      fontSize: theme.fontSizes.sm,
+      color:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[1]
+          : theme.colors.gray[7],
+      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+      borderRadius: theme.radius.sm,
+      fontWeight: 500,
+
+      "&:hover": {
+        backgroundColor:
+          theme.colorScheme === "dark"
+            ? theme.colors.dark[6]
+            : theme.colors.gray[0],
+        color: theme.colorScheme === "dark" ? theme.white : theme.black,
+
+        [`& .${icon}`]: {
+          color: theme.colorScheme === "dark" ? theme.white : theme.black,
+        },
+      },
+    },
+
+    linkIcon: {
+      ref: icon,
+      color:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[2]
+          : theme.colors.gray[6],
+      marginRight: theme.spacing.sm,
+    },
+
+    linkActive: {
+      "&, &:hover": {
+        backgroundColor: theme.fn.variant({
+          variant: "light",
+          color: theme.primaryColor,
+        }).background,
+        color: theme.fn.variant({ variant: "light", color: theme.primaryColor })
+          .color,
+        [`& .${icon}`]: {
+          color: theme.fn.variant({
+            variant: "light",
+            color: theme.primaryColor,
+          }).color,
+        },
+      },
+    },
+  }
+})
+
+function meals() {
+  const { classes, cx } = useStyles()
+  const [active, setActive] = useState("Billing")
+  const { docs: meals, lodaing, error } = useGetMeals()
+
+  return (
+    <Navbar height={700} width={{ sm: 300 }} p="md">
+      <Navbar.Section grow>
+        <Group className={classes.header} position="apart">
+          <Code sx={{ fontWeight: 700 }}>Your Meals</Code>
+        </Group>
+        {meals && meals.map((item) => (
+          <a
+            className={cx(classes.link, {
+              [classes.linkActive]: item.name === active,
+            })}
+            key={item.id}
+            onClick={(event) => {
+              event.preventDefault()
+              setActive(item.name)
+            }}
+          >
+            <span>{item.name}</span>
+          </a>
+        ))}
+        <Button className={cx(classes.link)}>New meal +</Button>
+      </Navbar.Section>
+
+      <Navbar.Section className={classes.footer}>
+        <a
+          href="#"
+          className={classes.link}
+          onClick={(event) => event.preventDefault()}
+        >
+          <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} />
+          <span>Change account</span>
+        </a>
+
+        <a
+          href="#"
+          className={classes.link}
+          onClick={(event) => event.preventDefault()}
+        >
+          <IconLogout className={classes.linkIcon} stroke={1.5} />
+          <span>Logout</span>
+        </a>
+      </Navbar.Section>
+    </Navbar>
+  )
+}
+
+export default meals
