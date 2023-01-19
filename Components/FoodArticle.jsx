4486e96d5da051c74b223d563091cfbb269804ec
robkarrr <93181892+robkarrr@users.noreply.github.com>
2023-01-19 20:11:04 +0100

    Styling for foodArticle

--- Components/FoodArticle.jsx
+++ Components/FoodArticle.jsx
@@ -1,20 +1,62 @@
+import {
+  createStyles,
+  Text,
+  Card,
+  RingProgress,
+  Group,
+  Button,
+  ActionIcon,
+} from "@mantine/core"
+import { IconPlus, IconTrash} from '@tabler/icons'
+
 function FoodArticle({ food, clear, add }) {
   return (
-    <div>
-      <ul>
-        <div key={food.id}>
-          <li>{food.name}</li>
-          <li>{food.calories}</li>
-          <li>{food.protein_g}</li>
-          <li>{food.serving_size_g}</li>
-        </div>
-      </ul>
-
+    <Card className="max-w-sm" withBorder p="md" radius="md">
       <div>
-        <button onClick={clear}>Clear</button>
-        <button onClick={add}>Add</button>
+        <div>
+          <Text size="xl">{food.serving_size_g}g {food.name}</Text>
+          <div>
+            <Text className="font-bold text-xl">{food.calories}</Text>
+            <Text size="xs" color="dimmed">
+              Calories
+            </Text>
+          </div>
+          <Group position={'apart'} mt="md">
+            <div>
+              <Text className="text-bold">{food.protein_g}g</Text>
+              <Text size="xs" color="dimmed">
+                Protein
+              </Text>
+            </div>
+
+            <div>
+              <Text className="text-bold">{food.fat_total_g}g</Text>
+              <Text size="xs" color="dimmed">
+                Fat
+              </Text>
+            </div>
+
+            <div>
+              <Text className="text-bold">{food.sugar_g}g</Text>
+              <Text size="xs" color="dimmed">
+                Sugar
+              </Text>
+            </div>
+
+            <Group>
+              <ActionIcon onClick={add}>
+                <IconPlus />
+              </ActionIcon>
+
+              <ActionIcon onClick={clear}>
+                <IconTrash />
+              </ActionIcon>
+            </Group>
+            
+          </Group>
+        </div>
       </div>
-    </div>
+    </Card>
   )
 }
 
