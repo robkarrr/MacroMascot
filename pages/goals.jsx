c4971a090d2fe46f2768bd3f7fdb2c7aaa5b1c31
robkarrr <93181892+robkarrr@users.noreply.github.com>
2023-01-22 11:10:17 +0100

    fixed bug

--- pages/goals.jsx
+++ pages/goals.jsx
@@ -15,8 +15,8 @@ import { doc, updateDoc } from "firebase/firestore"
 import { db } from "../firebase"
 import withAuth from "../middlewares/withAuth"
 import useGetUser from "../hooks/useGetUser"
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 import useGetDay from "../hooks/useGetDay"
 import moment from "moment"
 
@@ -64,10 +64,9 @@ const Goals = () => {
   const { classes } = useStyles()
   const { currentUser } = useAuthContext()
   const { data: userGoal } = useGetUser(currentUser.uid)
-  const [value, setValue] = useState(userGoal.calorie_goal)
+  const [value, setValue] = useState('1')
   const currentDate = moment().format("DD-MM-YY")
   const { docs: day } = useGetDay(currentDate)
-
 
   const newGoal = async () => {
     const ref = doc(db, `users/${currentUser.uid}`)
@@ -96,7 +95,7 @@ const Goals = () => {
         draggable: true,
         progress: undefined,
         theme: "light",
-        });
+      })
     }
   }
 
@@ -105,77 +104,68 @@ const Goals = () => {
       <Title mt={"md"} align="center">
         Your Goal
       </Title>
-      {userGoal.calorie_goal && (
-        <>
-          <div className={classes.wrapper}>
-            <NumberInput
-              mt={"md"}
-              value={value}
-              onChange={setValue}
-              label="What is your Goal"
-              placeholder="2200 is an average value"
-              defaultValue={userGoal.calorie_goal}
-              step={50}
-              min={1}
-              max={5000}
-              hideControls
-              classNames={{ input: classes.input, label: classes.label }}
-            />
-            <Slider
-              max={5000}
-              step={50}
-              min={1}
-              value={value}
-              defaultValue={userGoal.calorie_goal}
-              onChange={setValue}
-              size={2}
-              radius={0}
-              className={classes.slider}
-              classNames={{ thumb: classes.thumb, track: classes.track }}
-            />
-          </div>
-          <div align="center">
-            <Button mt={"md"} onClick={() => newGoal()} className="bg-blue-600">
-              Save!
-            </Button>
-          </div>
+      <>
+        <div className={classes.wrapper}>
+          <NumberInput
+            mt={"md"}
+            value={value}
+            onChange={setValue}
+            label="What is your Goal"
+            placeholder="2200 is an average value"
+            step={50}
+            min={1}
+            max={5000}
+            hideControls
+            classNames={{ input: classes.input, label: classes.label }}
+          />
+          <Slider
+            max={5000}
+            step={50}
+            min={1}
+            value={value}
+            onChange={setValue}
+            size={2}
+            radius={0}
+            className={classes.slider}
+            classNames={{ thumb: classes.thumb, track: classes.track }}
+          />
+        </div>
+        <div align="center">
+          <Button mt={"md"} onClick={() => newGoal()} className="bg-blue-600">
+            Save!
+          </Button>
+        </div>
 
-          {userGoal.calorie_goal && day.total && (
-                <Card
-                  mt={'lg'}
-                  withBorder
-                  radius="md"
-                  p="xl"
-                  sx={(theme) => ({
-                    backgroundColor:
-                      theme.colorScheme === "dark"
-                        ? theme.colors.dark[7]
-                        : theme.white,
-                  })}
-                >
-                  <Text
-                    size="xs"
-                    transform="uppercase"
-                    weight={700}
-                    color="dimmed"
-                  >
-                    Daily Goal
-                  </Text>
-                  <Text size="lg" weight={500}>
-                    {day.total.calories} / {userGoal.calorie_goal}
-                  </Text>
-                  <Progress
-                    value={Math.ceil(
-                      (day.total.calories / userGoal.calorie_goal) * 100
-                    )}
-                    mt="md"
-                    size="lg"
-                    radius="xl"
-                  />
-                </Card>
+        {userGoal.calorie_goal && day?.total && (
+          <Card
+            mt={"lg"}
+            withBorder
+            radius="md"
+            p="xl"
+            sx={(theme) => ({
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[7]
+                  : theme.white,
+            })}
+          >
+            <Text size="xs" transform="uppercase" weight={700} color="dimmed">
+              Daily Goal
+            </Text>
+            <Text size="lg" weight={500}>
+              {day.total.calories} / {userGoal.calorie_goal}
+            </Text>
+            <Progress
+              value={Math.ceil(
+                (day.total.calories / userGoal.calorie_goal) * 100
               )}
-        </>
-      )}
+              mt="md"
+              size="lg"
+              radius="xl"
+            />
+          </Card>
+        )}
+      </>
       <ToastContainer
         position="top-right"
         autoClose={5000}
